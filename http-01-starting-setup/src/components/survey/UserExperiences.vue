fde6b88fbf38be16fbdf70ef4aa2e34e80f88319
minTwin <michaelmin2020@gmail.com>
2022-03-04 07:48:31 -0600

    replaced fetch with axios.get API call to firebase

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -21,49 +21,48 @@
 
 <script>
 import SurveyResult from './SurveyResult.vue';
-//import axios from 'axios';
+import axios from 'axios';
 
 export default {
   components: {
     SurveyResult,
   },
   data() {
-    return{
-      results: []
-    }
+    return {
+      results: [],
+    };
   },
-  methods:{
+  methods: {
     loadExperiences() {
-      fetch('https://vue-http-demo-b99c4-default-rtdb.firebaseio.com/surveys.json')
+      //fetch('https://vue-http-demo-b99c4-default-rtdb.firebaseio.com/surveys.json')
       //axios.get('https://vue-http-demo-b99c4-default-rtdb.firebaseio.com/surveys.json')
-      .then((response) => {
-        if(response.ok) {
-          return response.json();
-        }
-      })
-      .then((data) => {
-        const results = [];
-        for (const id in data) {
-          results.push({ 
-            id: id, 
-            name: data[id].name,
-            rating: data[id].rating 
-          });
-        }
-        this.results = results;
-      });
+      // .then((response) => {
+      //   if(response.ok) {
+      //     return response.json();
+      //   }
+      // })
+      // .then((data) => {
+      //   const results = [];
+      //   for (const id in data) {
+      //     results.push({
+      //       id: id,
+      //       name: data[id].name,
+      //       rating: data[id].rating
+      //     });
+      //   }
+      //   this.results = results;
+      // });
+      axios
+        .get('https://vue-http-demo-b99c4-default-rtdb.firebaseio.com/surveys.json')
+          .then(({ data }) => {
+            this.results = Object.entries(data).map(([id, { name, rating }]) => ({ id, name, rating, }));
+        });
     },
   },
   mounted() {
-    this.loadExperiences();
-  }
+    //this.loadExperiences();
+  },
 };
-
-  // mounted(){
-  //   axios.get('https://vue-http-demo-b99c4-default-rtdb.firebaseio.com/surveys.json').then(response => {      
-  //     this.results = response.data;
-  //   })
-  // },
 
 </script>
 
